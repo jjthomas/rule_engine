1d486d5041c93887d203fbda37a1ace784b5f5be
James Thomas <jamesjoethomas@gmail.com>
2020-10-29 00:40:24 -0700

    mac os support

--- build.sh
+++ build.sh
@@ -2,5 +2,8 @@
 python3 -c 'import pyarrow; pyarrow.create_library_symlinks()'
 INC=$(python3 -c 'import pyarrow; print(pyarrow.get_include())')
 LIB=$(python3 -c 'import pyarrow; print(pyarrow.get_library_dirs()[0])')
-g++ -I$INC -I/usr/include/python3.6m -fPIC cube.cpp -shared -o libcube.so -L$LIB -larrow -larrow_python
+PY_INC=$(python3 -c "from sysconfig import get_paths as gp; print(gp()['include'])")
+PY_LIB_NAME=$(basename $PY_INC)
+PY_LIB=$(dirname $(python3 -c "from sysconfig import get_paths as gp; print(gp()['stdlib'])"))
+g++ -std=c++11 -I$INC -I$PY_INC -fPIC cube.cpp -shared -o libcube -L$LIB -L$PY_LIB -larrow -larrow_python -l$PY_LIB_NAME
 
--- cube.py
+++ cube.py
@@ -2,7 +2,7 @@ import ctypes
 import pandas as pd
 import pyarrow as pa
 
-c_lib = ctypes.CDLL("./libcube.so")
+c_lib = ctypes.CDLL("./libcube")
 df = pd.DataFrame({"a": [1, 2, 3]})
 table = pa.Table.from_pandas(df)
 c_lib.print_is_array(ctypes.py_object(table))
