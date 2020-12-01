b7254237b5eacf47660d1a4a900ddc6c081992a4
James Thomas <jamesjoethomas@gmail.com>
2020-12-01 15:56:01 -0800

    filter rookies dataset

--- cube.py
+++ cube.py
@@ -3,8 +3,8 @@ import pandas as pd
 import pyarrow as pa
 
 c_lib = ctypes.CDLL("./libcube")
-# df = pd.DataFrame({"a": [1, 2, 3]})
 df = pd.read_csv("Rookies.csv")
+df = df[df["Year"] >= 1995]
 table = pa.Table.from_pandas(df)
 c_lib.compute_stats(ctypes.py_object(table), df.columns.get_loc("IFAS"),
   ctypes.c_double(3.0), 20)
