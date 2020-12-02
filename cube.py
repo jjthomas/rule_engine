4a598d62525f0174869be3efd1eb0e40d3088634
James Thomas <jamesjoethomas@gmail.com>
2020-12-02 14:20:11 -0800

    doc on args

--- cube.py
+++ cube.py
@@ -6,6 +6,7 @@ c_lib = ctypes.CDLL("./libcube")
 df = pd.read_csv("Rookies.csv")
 # df = df[df["Year"] >= 1995]
 table = pa.Table.from_pandas(df)
+# args: dataset, metric column idx, z_thresh, count_thresh, show_nulls
 c_lib.compute_stats(ctypes.py_object(table), df.columns.get_loc("IFAS"),
   ctypes.c_double(3.0), 20, ctypes.c_bool(False))
 
