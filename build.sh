516fbe8acefc6795e5fb90a1ce36278aaf4f4e49
James Thomas <jamesjoethomas@gmail.com>
2020-12-02 20:33:48 -0800

    remove python link lib for Linux

--- build.sh
+++ build.sh
@@ -3,7 +3,11 @@ python3 -c 'import pyarrow; pyarrow.create_library_symlinks()'
 INC=$(python3 -c 'import pyarrow; print(pyarrow.get_include())')
 LIB=$(python3 -c 'import pyarrow; print(pyarrow.get_library_dirs()[0])')
 PY_INC=$(python3 -c "from sysconfig import get_paths as gp; print(gp()['include'])")
-PY_LIB_NAME=$(basename $PY_INC)
-PY_LIB=$(dirname $(python3 -c "from sysconfig import get_paths as gp; print(gp()['stdlib'])"))
-g++ -std=c++11 -I$INC -I$PY_INC -fPIC cube.cpp -shared -o libcube -L$LIB -L$PY_LIB -larrow -larrow_python -l$PY_LIB_NAME
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  PY_LIB_NAME=$(basename $PY_INC)
+  PY_LIB_NAME="-l$PY_LIB_NAME"
+  PY_LIB=$(dirname $(python3 -c "from sysconfig import get_paths as gp; print(gp()['stdlib'])"))
+  PY_LIB="-L$PY_LIB"
+fi
+g++ -std=c++11 -I$INC -I$PY_INC -fPIC cube.cpp -shared -o libcube -L$LIB $PY_LIB -larrow -larrow_python $PY_LIB_NAME
 
