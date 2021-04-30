3bac06f43a65eb51339213597bb25610df6d3cae
James Thomas <jamesjoethomas@gmail.com>
2021-04-30 15:53:51 -0700

    removed AS column from Rookies

--- rookies.py
+++ rookies.py
@@ -2,7 +2,6 @@ import cube
 import pandas as pd
 
 df = pd.read_csv("Rookies.csv")
-df = df.drop(columns = ['AS'])
 s = cube.compute_sums(df, "IFAS")
 r = s.get_rules(0.7, 10)
 print(s.evaluate_summary(df, r))
