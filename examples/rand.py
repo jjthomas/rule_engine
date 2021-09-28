a842efbd052e79ea8d720350e2b293b5fca96e8f
James Thomas <jamesjoethomas@gmail.com>
2021-09-28 11:36:59 -0700

    multiple random data distributions

--- examples/rand.py
+++ examples/rand.py
@@ -1,4 +1,4 @@
-# python rand.py <num cols> <upper bound of column values, non-inclusive>
+# python rand.py <num cols> <optional: power distribution parameter>
 import rule_engine as re
 import pandas as pd
 import numpy as np
@@ -7,8 +7,16 @@ import sys
 df = pd.DataFrame()
 np.random.seed(0)
 df["c0"] = np.random.randint(0, 2, 1 << 22)
+use_power = len(sys.argv) > 2
+if use_power:
+  a = int(sys.argv[2])
 for i in range(1, int(sys.argv[1])):
-  df["c%d" % i] = np.random.randint(0, int(sys.argv[2]), 1 << 22)
+  if use_power:
+    data = np.random.power(a, 1 << 22)
+    data[0] = 0 # ensure range is 0.0 to 1.0
+  else:
+    data = np.random.rand(1 << 22)
+  df["c%d" % i] = data
 s = re.compute_sums(df, "c0")
 print(s.get_rules(0.50, 10))
 
