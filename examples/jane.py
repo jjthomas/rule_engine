cc385b6bc29bd18afe36d3fb8ff614ff15796a10
James Thomas <jamesjoethomas@gmail.com>
2021-10-04 23:24:56 +0000

    multiple iterations within python examples

--- examples/jane.py
+++ examples/jane.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import rule_engine as re
+import sys
 
 df = pd.read_parquet("train.parquet")
 df["resp_bin"] = (df["resp"] > 0.15).astype(int)
 df = df.drop(columns=['resp', 'resp_1', 'resp_2', 'resp_3', 'resp_4'])
-s = re.compute_sums(df, "resp_bin")
+for i in range(int(sys.argv[1])):
+  s = re.compute_sums(df, "resp_bin")
 print(len(s.get_rules(0.9, 10)))
--- examples/rand.py
+++ examples/rand.py
@@ -1,4 +1,4 @@
-# python rand.py <num cols> <optional: power distribution parameter>
+# python rand.py <num cols> <num trials> <optional: power distribution parameter>
 import rule_engine as re
 import pandas as pd
 import numpy as np
@@ -7,9 +7,9 @@ import sys
 df = pd.DataFrame()
 np.random.seed(0)
 df["c0"] = np.random.randint(0, 2, 1 << 22)
-use_power = len(sys.argv) > 2
+use_power = len(sys.argv) > 3
 if use_power:
-  a = int(sys.argv[2])
+  a = int(sys.argv[3])
 for i in range(1, int(sys.argv[1])):
   if use_power:
     data = np.random.power(a, 1 << 22)
@@ -17,6 +17,7 @@ for i in range(1, int(sys.argv[1])):
   else:
     data = np.random.rand(1 << 22)
   df["c%d" % i] = data
-s = re.compute_sums(df, "c0")
+for i in range(int(sys.argv[2])):
+  s = re.compute_sums(df, "c0")
 print(s.get_rules(0.50, 10))
 
